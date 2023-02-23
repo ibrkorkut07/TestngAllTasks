e28185ffe3d964bf51e90a538ed90c0e36f3676e
ibrkorkut07 <ibr.kor@gmail.com>
2023-02-23 22:30:17 +0100

    v1 restored

--- src/test/java/pages/V1.java
+++ src/test/java/pages/V1.java
@@ -1,7 +1,5 @@
 package pages;
 
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
@@ -11,9 +9,5 @@ public class V1 {
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
-    @FindBy(id="twotabsearchtextbox")
-    public WebElement amazonAramaKutusu;
-
-    @FindBy(xpath = "//div[@class='a-section a-spacing-small a-spacing-top-small']")
-    public WebElement sonucYazisiElementi;
+    
 }
